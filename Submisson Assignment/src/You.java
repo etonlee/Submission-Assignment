0e3d787884dbe4ae080b601b44b2126fb7bb45dd
etonlee <51206420+etonlee@users.noreply.github.com>
2019-06-06 13:24:55 -0400

    You.java
    This is a submission assignment with information about me. Increases age, gets name, and tells you happy birthday and give you favorite animal

--- Submisson Assignment/src/You.java
+++ Submisson Assignment/src/You.java
@@ -18,13 +18,40 @@
  *  - Add achiu-frhsd as a collaborator
  *
  */
-public class You{
+public class You
+{
 
 	private String firstName;
 	private String lastName;
 	private String favoriteAnimal;
 	private int age;
 
-	// Write your code here
+	// Write your code here.
+	
+	public You()
+	{
+		firstName = "Ethan";
+		lastName = "Lee";
+		favoriteAnimal = "Ishma";
+		age = 17;
+	}
+	
+	public String getFullName()
+	{
+		return firstName + " " + lastName;
+	}
+	
+	public String setFavoriteAnimal(String animal)
+	{
+		favoriteAnimal = animal;
+		return favoriteAnimal;
+	}
+	
+	public int happyBirthday()
+	{
+		System.out.println("Happy Birthday!");
+		age++;
+		return age;
+	}
 	
 }
